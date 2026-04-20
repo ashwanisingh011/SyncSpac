f86ef261ca3bf74159df263dd48264ad8bca36e4
ashwanisingh011 <ashwanisingh9737@gmail.com>
2026-04-20 23:46:53 +0530

    sign in api

--- backend/src/controllers/authControllers.ts
+++ backend/src/controllers/authControllers.ts
@@ -1,39 +1,74 @@
 import User from "../models/User.js";
-import type {Request, Response} from 'express';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import type { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-export const registerUser = (async (req: Request, res: Response): Promise<void>=>{
-try {
-        const{name, email, password} = req.body;
-    
-        const existedUser = await User.findOne({email})
-        if(existedUser){
-            res.status(400).json({message: "User already exists"})
-            return 
-        }
-    
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password, salt)
-    
-       const user = await User.create({
-        name,
-        email,
-        password: hashedPassword, 
-       })
-    
-        res.status(201).json({  
-            message: "User registered successfully",
-            user: {
-                id: user._id,
-                name: user.name,
-                email: user.email,
-                avator: user.avatar
-            }
-    })
-} catch (error) {
-    console.error("Error in registerUser: ", error)
-    res.status(500).json({message: "Server error"})
-}
-    
-})
+export const registerUser = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existedUser = await User.findOne({ email });
+    if (existedUser) {
+      res.status(400).json({ message: "User already exists" });
+      return;
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar,
+      },
+    });
+  } catch (error) {
+    console.error("Error in registerUser: ", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const loginUser = async (req: Request, res: Response): Promise<void> => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).json({ message: "User does not exist" });
+      return;
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      res.status(400).json({ message: "Invalid credentials" });
+      return;
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET as string, {
+      expiresIn: "7d",
+    });
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar,
+      },
+    });
+  } catch (error) {
+    console.error("Error in loginUser: ", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
