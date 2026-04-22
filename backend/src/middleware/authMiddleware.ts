a4d986ff17ce3330badbcb0c4b268203878bd66d
ashwanisingh011 <ashwanisingh9737@gmail.com>
2026-04-23 00:45:43 +0530

    added a auth middleware

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -2,5 +2,22 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 export interface AuthRequest extends Request {
-    user?: any; 
+    userId?: any;
+}
+
+export const protect = async (req: AuthRequest, res: Response, next: NextFunction): Promise<void> => {
+    const authHeader = req.headers.authorization;
+    if(!authHeader ||  !authHeader.startsWith('Bearer')){
+        res.status(401).json({message: "Not authorized, no token"});
+        return;
+    }
+    const token = authHeader.split(' ')[1];
+    try {
+        const decoded = jwt.verify(token as string, process.env.JWT_SECRET as string)
+        req.userId = decoded
+        next()
+    }catch(error){
+        console.error(error)
+        res.status(401).json({ message: "Not authorized, token failed" });
+    }
 }
