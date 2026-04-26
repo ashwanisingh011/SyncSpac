652119c45ff2763daa4562ebd19545943d2fa448
ashwanisingh011 <ashwanisingh9737@gmail.com>
2026-04-26 21:51:28 +0530

    Hooked workspace

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
+import workspaceRoutes from './routes/workspaceRoutes.js';
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/workspace', workspaceRoutes)
 
 
 connectDB()
