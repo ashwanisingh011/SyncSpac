0041012aeb93ee38b47772e31fdc6220ffa4021a
ashwanisingh011 <ashwanisingh9737@gmail.com>
2026-05-01 23:09:36 +0530

    added a get workspace routes

--- backend/src/routes/workspaceRoutes.ts
+++ backend/src/routes/workspaceRoutes.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import { createWorkspace } from '../controllers/workspaceControllers.js';
+import { createWorkspace, getWorkspace } from '../controllers/workspaceControllers.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/', protect, createWorkspace)
-
+router.get ('/:id', protect, getWorkspace)
 export default router;
