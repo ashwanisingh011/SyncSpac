826eb10d7e8d849a15af72feb4aebc268ebdd14a
ashwanisingh011 <ashwanisingh9737@gmail.com>
2026-05-01 23:08:14 +0530

    added a get workspace route function

--- backend/src/controllers/workspaceControllers.ts
+++ backend/src/controllers/workspaceControllers.ts
@@ -31,3 +31,20 @@ export const createWorkspace = async (req: AuthRequest, res: Response): Promise<
         res.status(500).json({ message: "Server error" });
     }
 };
+
+export const getWorkspace = async (req: AuthRequest, res: Response): Promise<void> => {
+    const workspaceId = req.params.id;
+
+    try{
+        const workspace = await Workspace.findById(workspaceId).populate('projects')
+        if(!workspace){
+            res.status(404).json({message: "Workspace not found"});
+            return;
+        }
+        res.status(200).json(workspace);
+    }
+    catch(error){
+        console.error("Error fetching workspace:", error);
+        res.status(500).json({message: "server error"})
+    }
+}
