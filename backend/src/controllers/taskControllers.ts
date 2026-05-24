9eb7ab621a35cd8bddbc0436634bcf36a91f605e
ashwanisingh011 <ashwanisingh9737@gmail.com>
2026-05-24 23:22:23 +0530

    feat: implement updateTaskStatus functionality

--- backend/src/controllers/taskControllers.ts
+++ backend/src/controllers/taskControllers.ts
@@ -32,3 +32,33 @@ export const createTask = async(req: AuthRequest, res: Response):Promise<void> =
         res.status(500).json({message: "Server error"});
     }
 }
+
+export const updateTaskStatus = async(req: AuthRequest, res: Response):Promise<void> => {
+    try {
+     const {taskId} = req.params;
+     const {newStatus, projectId} = req.body;
+
+     const task = await Task.findById(taskId);
+     if(!task){
+        res.status(404).json({message: "Task not found"});
+        return;
+     }
+
+     const oldStatus = task.status;
+
+     task.status = newStatus;
+     await task.save();
+
+     await Project.findByIdAndUpdate(projectId, {$pull: {[`columns.${oldStatus}`]: task._id}})
+     await Project.findByIdAndUpdate(projectId, {$push: {[`columns.${newStatus}`]: task._id}})
+
+     res.status(200).json({
+        message: "Task status updated successfully",
+        task
+     })
+     
+    } catch (error) {
+        console.error("Error updating task status:", error);
+        res.status(500).json({message: "Server error"});
+    }
+}
