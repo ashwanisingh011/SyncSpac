678d7df7a4bc2080f09490805105fc9f159e444f
ashwanisingh011 <ashwanisingh9737@gmail.com>
2026-05-30 23:44:15 +0530

    feat: add updateTaskStatus route to taskRoutes

--- backend/src/routes/taskRoutes.ts
+++ backend/src/routes/taskRoutes.ts
@@ -1,9 +1,10 @@
     import express from 'express';
-    import { createTask } from '../controllers/taskControllers.js';
+    import { createTask, updateTaskStatus } from '../controllers/taskControllers.js';
     import { protect } from '../middleware/authMiddleware.js';
 
     const router = express.Router();
 
     router.post('/', protect, createTask)
+    router.put('/:taskId/status', protect, updateTaskStatus)
 
     export default router;  
