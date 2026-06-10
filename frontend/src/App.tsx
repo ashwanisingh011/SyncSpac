4a59218d781dd3a2b96c039d9e287e7083f073ee
ashwanisingh011 <ashwanisingh9737@gmail.com>
2026-06-11 00:01:21 +0530

    feat: replace KhanbanBoard with routing for Login, Dashboard, and ProjectPage components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
-import KhanbanBoard from './components/KanbanBoard'
-
-
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import ProjectPage from './components/ProjectPage.tsx';
+import Login from './components/Login.tsx';
+import Dashboard from './components/Dashboard.tsx';
 
 
 function App() {
 
   return (
     <>
-      <KhanbanBoard project={} />
+    <BrowserRouter>
+      <Routes>
+        <Route path = '/login' element={<Login />} />
+        <Route path = '/dashboard' element={<Dashboard />} />
+        <Route path = "/project/:id" element={<ProjectPage />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
