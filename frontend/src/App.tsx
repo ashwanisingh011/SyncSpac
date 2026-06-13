149387675525caca9723e6736f6edd2037529952
ashwanisingh011 <ashwanisingh9737@gmail.com>
2026-06-13 23:24:03 +0530

    feat: update routing in App component for Login, Register, and CreateProject

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import ProjectPage from './components/ProjectPage.tsx';
+import CreateProject from './components/CreateProject.tsx';
 import Login from './components/Login.tsx';
+import Register from './components/Register.tsx';
 import Dashboard from './components/Dashboard.tsx';
 
 
@@ -10,8 +12,10 @@ function App() {
     <>
     <BrowserRouter>
       <Routes>
-        <Route path = '/login' element={<Login />} />
+        <Route path = '/' element={<Login />} />
+        <Route path = '/register' element={<Register />} />
         <Route path = '/dashboard' element={<Dashboard />} />
+        <Route path = '/project' element={<CreateProject />} />
         <Route path = "/project/:id" element={<ProjectPage />} />
       </Routes>
     </BrowserRouter>
