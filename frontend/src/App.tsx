e1a8b199387ba5e0f09f5e5f91f8611b905c6c89
ashwanisingh011 <ashwanisingh9737@gmail.com>
2026-06-26 00:10:20 +0530

    fix: correct route path for Login component in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
     <>
     <BrowserRouter>
       <Routes>
-        <Route path = '/' element={<Login />} />
+        <Route path = '/login' element={<Login />} />
         <Route path = '/register' element={<Register />} />
 
         <Route path = '/dashboard' element={
